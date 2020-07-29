cb27718381d80a498b449ce4cf1c9b9015bf45a3
shanejbb <shanejabbari@gmail.com>
2020-07-29 16:42:03 +0430

    Backend infrastructure built

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //Return either home or auth widget based on user's status
+    return Container();
+  }
+}
