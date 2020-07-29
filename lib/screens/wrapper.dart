513aab7f1287ce0880b66e172102ea90155d7e00
shanejbb <shanejabbari@gmail.com>
2020-07-29 16:43:09 +0430

    backend infrstracture upgrading

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hangout/screens/home/home.dart';
 
 class wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Return either home or auth widget based on user's status
-    return Container();
+    return home();
   }
 }
