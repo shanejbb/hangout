76640f5345c3b2aea82a927c060dc07b69a820b4
shanejbb <shanejabbari@gmail.com>
2020-07-29 16:46:26 +0430

    backend infrstracture upgrading

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hangout/screens/wrapper.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,5 +7,9 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: wrapper(),
+    );
+  }
 }
